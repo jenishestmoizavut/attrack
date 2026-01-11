a2941dbe2bab8384320960e57eb958ba5e11463e
Jenish <jenishschaplot@gmail.com>
2026-01-11 10:23:44 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "v1.1.2"; // Bumped version to force update
+const CACHE_VERSION = "v1.1.3"; // Bumped version to force update
 const CACHE_NAME = `attrack-${CACHE_VERSION}`;
 
 const CORE_ASSETS = [
@@ -77,3 +77,4 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
